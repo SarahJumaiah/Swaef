7336ba1dccb9f2723189e35911f21e5638aa53bc
HA <ha@HAs-MacBook-Pro.local>
2024-10-22 11:39:02 +0300

    change sign , login design

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import logo from '../assets/logo.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Sign() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-center w-full min-h-screen bg-gray-100">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-4xl w-full flex flex-col md:flex-row">
-        
- <div className="md:w-1/2 flex flex-col items-center justify-center bg-white p-4 rounded-lg mb-8 md:mb-0">
-
-  <img src={logo} alt="Logo" className="w-28 h-auto mb-4" />
-
-  <div className="text-center ">
-    <h3 className="text-xl font-bold text-[#ab1c1c] mb-2">انضم كمسعف تطوعي</h3>
-    <p className="text-gray-600 text-base leading-relaxed">
-      كن جزءًا من فريق المسعفين التطوعي وساهم في تعزيز الرعاية الصحية. دورك كمسعف سيساعد في إنقاذ الأرواح ودعم المجتمع في أوقات الحاجة.
-    </p>
-  </div>
-</div>
+      <div className="p-8 max-w-4xl w-full flex flex-col md:flex-row gap-8">
 
 
+        <div className="md:w-1/2 flex flex-col items-center justify-center space-y-6 mb-8 md:mb-0">
+          <img src={logo} alt="Logo" className="w-32 h-auto mb-4" />
 
-        <div className="md:w-1/2">
-          {/* <h2 className="text-3xl font-bold text-center text-[#ab1c1c] mb-6">انضمام المسعفين</h2> */}
+          <div className="text-center">
+            <h3 className="text-2xl font-bold text-[#ab1c1c] mb-2">انضم كمسعف تطوعي</h3>
+            <p className="text-gray-600 text-base leading-relaxed">
+              كن جزءًا من فريق المسعفين التطوعي وساهم في تعزيز الرعاية الصحية. دورك كمسعف سيساعد في إنقاذ الأرواح ودعم المجتمع في أوقات الحاجة.
+            </p>
+          </div>
+        </div>
 
-          <form className="rtl">
 
-            <div className="mb-4 border-b-2 border-[#ab1c1c]">
+        <div className="md:w-1/2 bg-white shadow-lg rounded-xl p-6 space-y-6">
+          <form className="rtl space-y-4">
+
+            <div className="border-b-2 border-[#ab1c1c]">
               <input
                 type="text"
                 id="name"
@@ -38,7 +37,7 @@ function Sign() {
             </div>
 
 
-            <div className="mb-4 border-b-2 border-[#ab1c1c]">
+            <div className="border-b-2 border-[#ab1c1c]">
               <input
                 type="email"
                 id="email"
@@ -50,7 +49,7 @@ function Sign() {
             </div>
 
 
-            <div className="mb-4 border-b-2 border-[#ab1c1c]">
+            <div className="border-b-2 border-[#ab1c1c]">
               <input
                 type="password"
                 id="password"
@@ -61,8 +60,8 @@ function Sign() {
               />
             </div>
 
-
-            <div className="mb-6">
+            {/* File Upload */}
+            <div>
               <label
                 htmlFor="file-upload"
                 className="w-full cursor-pointer p-3 border-b-2 border-[#ab1c1c] bg-transparent text-gray-700 flex justify-between items-center"
@@ -85,6 +84,7 @@ function Sign() {
               />
             </div>
 
+
             <div className="mt-6">
               <button
                 type="submit"
@@ -96,7 +96,8 @@ function Sign() {
           </form>
 
           <div className="mt-4 text-sm text-center">
-            <p className="text-gray-600">لديك حساب؟
+            <p className="text-gray-600">
+              لديك حساب؟ 
               <Link to="/login" className="text-[#ab1c1c] hover:underline"> تسجيل دخول</Link>
             </p>
           </div>
